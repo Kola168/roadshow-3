71bca57cd2d0d4e3d75366e47dd31f7f708c8137
011413 <caiyang@htsc.com>
2017-09-27 07:38:56 +0000

    #LINKAPP-30#add chapter4 5 7

--- src/components/handbook/content/chapters.js
+++ src/components/handbook/content/chapters.js
@@ -1,5 +1,11 @@
 import chapter3 from '../../../json/chapters/chapter3.json';
+import chapter4 from '../../../json/chapters/chapter4.json';
+import chapter5 from '../../../json/chapters/chapter5.json';
+import chapter7 from '../../../json/chapters/chapter7.json';
 
 export default{
     chapter3,
+    chapter4,
+    chapter5,
+    chapter7
 }
